d905ba0a52be9eaa8c100d50654d3e3ef6284a27
廖凯 <17377787801@189.cn>
2023-06-28 23:57:08 +0800

    Init

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	url := "https://bbs.hupu.com/bxj"
+	selector := ".p-title"
+
+	results, err := scrapeUrl(url, selector)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, result := range results {
+		fmt.Println(result)
+	}
+}
+
+func scrapeUrl(url string, selector string) ([]string, error) {
+	// 发送HTTP GET请求并获取网页内容
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(res.Body)
+
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("failed to fetch webpage: %s", res.Status)
+	}
+
+	// 使用goquery解析HTML
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// 提取指定的HTML标签中的内容
+	var results []string
+	doc.Find(selector).Each(func(i int, s *goquery.Selection) {
+		results = append(results, s.Text())
+	})
+
+	return results, nil
+}
